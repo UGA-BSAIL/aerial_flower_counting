c96f767fede00b3da6b1848ff47552e8380feee9
Daniel Petti <djpetti@gmail.com>
2020-09-22 08:58:32 -0400

    Decrease batch normalization momentum.

--- src/cotton_counter/pipelines/model_training/model/sa_net.py
+++ src/cotton_counter/pipelines/model_training/model/sa_net.py
@@ -2,6 +2,8 @@
 Implements the SaNet model architecture.
 """
 
+from functools import partial
+
 import tensorflow as tf
 import tensorflow.keras as keras
 import tensorflow.keras.backend as K
@@ -34,33 +36,35 @@ def build_model(*, input_size: Vector2I) -> keras.Model:
         float_images
     )
 
+    batch_norm = partial(layers.BatchNormalization, momentum=0.5)
+
     # Main convolution layers.
     conv1_1 = layers.Conv2D(64, 3, padding="same", activation="relu")(
         normalized
     )
-    norm1_1 = layers.BatchNormalization()(conv1_1)
+    norm1_1 = batch_norm()(conv1_1)
     conv1_2 = layers.Conv2D(64, 3, padding="same", activation="relu")(norm1_1)
-    norm1_2 = layers.BatchNormalization()(conv1_2)
+    norm1_2 = batch_norm()(conv1_2)
     pool1 = layers.MaxPool2D()(norm1_2)
 
     conv2_1 = layers.Conv2D(128, 3, padding="same", activation="relu")(pool1)
-    norm2_1 = layers.BatchNormalization()(conv2_1)
+    norm2_1 = batch_norm()(conv2_1)
     conv2_2 = layers.Conv2D(128, 3, padding="same", activation="relu")(norm2_1)
-    norm2_2 = layers.BatchNormalization()(conv2_2)
+    norm2_2 = batch_norm()(conv2_2)
     pool2 = layers.MaxPool2D()(norm2_2)
 
     conv3_1 = layers.Conv2D(256, 3, padding="same", activation="relu")(pool2)
-    norm3_1 = layers.BatchNormalization()(conv3_1)
+    norm3_1 = batch_norm()(conv3_1)
     conv3_2 = layers.Conv2D(256, 3, padding="same", activation="relu")(norm3_1)
-    norm3_2 = layers.BatchNormalization()(conv3_2)
+    norm3_2 = batch_norm()(conv3_2)
     conv3_3 = layers.Conv2D(256, 3, padding="same", activation="relu")(norm3_2)
-    norm3_3 = layers.BatchNormalization()(conv3_3)
+    norm3_3 = batch_norm()(conv3_3)
     pool3 = layers.MaxPool2D()(norm3_3)
 
     conv4_1 = layers.Conv2D(512, 3, padding="same", activation="relu")(pool3)
-    norm4_1 = layers.BatchNormalization()(conv4_1)
+    norm4_1 = batch_norm()(conv4_1)
     conv4_2 = layers.Conv2D(512, 3, padding="same", activation="relu")(norm4_1)
-    norm4_2 = layers.BatchNormalization()(conv4_2)
+    norm4_2 = batch_norm()(conv4_2)
 
     # Add the projection layers.
     density_map = layers.Conv2D(1, 1, name="density_map")(norm4_2)
