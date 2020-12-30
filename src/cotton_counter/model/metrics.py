a0bead27cba1bf1e1625db3709a9e5d90de490e7
Daniel Petti <djpetti@gmail.com>
2020-12-30 11:02:56 -0800

    Add some more meaningful metrics.
    Raw accuracy isn't very useful in this case since the data is rather unbalanced.

--- src/cotton_counter/model/metrics.py
+++ src/cotton_counter/model/metrics.py
@@ -3,14 +3,14 @@ Custom keras metrics.
 """
 
 
-from typing import Dict, Union
+from typing import Dict, List, Union
 
 from tensorflow import keras
 
 
 def make_metrics(
     classify_counts: bool = False,
-) -> Dict[str, Union[str, keras.metrics.Metric]]:
+) -> Dict[str, Union[str, List[keras.metrics.Metric]]]:
     """
     Creates the metrics dictionary to use when compiling a model.
 
@@ -25,6 +25,17 @@ def make_metrics(
 
     if classify_counts:
         # Add a standard accuracy metric for the classification.
-        metrics["discrete_count"] = "binary_accuracy"
+        metrics["discrete_count"] = [
+            keras.metrics.BinaryAccuracy(name="accuracy"),
+            # Reverse the names of these metrics because we use 0 as the
+            # positive class.
+            keras.metrics.TruePositives(name="tn"),
+            keras.metrics.TrueNegatives(name="tp"),
+            keras.metrics.FalsePositives(name="fn"),
+            keras.metrics.FalseNegatives(name="fp"),
+            keras.metrics.Precision(name="precision"),
+            keras.metrics.Recall(name="recall"),
+            keras.metrics.AUC(name="auc"),
+        ]
 
     return metrics
