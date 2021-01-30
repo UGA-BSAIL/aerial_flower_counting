111311dfa26b7b9d989a11db63c716251e87d4f5
Daniel Petti <djpetti@gmail.com>
2021-01-30 12:52:08 -0500

    Add AlexNet-based model.

--- src/cotton_counter/model/counting_model.py
+++ src/cotton_counter/model/counting_model.py
@@ -118,6 +118,35 @@ def _build_le_net_backbone(normalized_input: tf.Tensor) -> tf.Tensor:
     return conv3_2
 
 
+def _build_alexnet_backbone(normalized_input: tf.Tensor) -> tf.Tensor:
+    """
+    Creates a model similar to the AlexNet-based architecture used in the
+    TasselNet paper. Note, however, that the input size is much larger.
+
+    Args:
+        normalized_input: The normalized input images.
+
+    Returns:
+        The top model layer.
+
+    """
+    conv1_1 = _conv_bn_relu(16, 3, padding="same")(normalized_input)
+    pool1 = layers.MaxPool2D()(conv1_1)
+
+    conv2_1 = _conv_bn_relu(32, 3, padding="same")(pool1)
+    pool2 = layers.MaxPool2D()(conv2_1)
+
+    conv3_1 = _conv_bn_relu(64, 3, padding="same")(pool2)
+    conv3_2 = _conv_bn_relu(64, 3, padding="same")(conv3_1)
+    conv3_3 = _conv_bn_relu(64, 3, padding="same")(conv3_2)
+    pool3 = layers.MaxPool2D()(conv3_3)
+
+    conv4_1 = _conv_bn_relu(128, 3, padding="same")(pool3)
+    conv4_2 = _conv_bn_relu(128, 3, padding="same")(conv4_1)
+
+    return conv4_2
+
+
 def _build_model_backbone(*, image_input: keras.Input) -> tf.Tensor:
     """
     Creates the backbone of the model.
@@ -133,7 +162,7 @@ def _build_model_backbone(*, image_input: keras.Input) -> tf.Tensor:
     float_images = tf.cast(image_input, K.floatx())
     normalized = tf.image.per_image_standardization(float_images)
 
-    return _build_le_net_backbone(normalized)
+    return _build_alexnet_backbone(normalized)
 
 
 def _build_density_map_head(model_top: tf.Tensor) -> tf.Tensor:
