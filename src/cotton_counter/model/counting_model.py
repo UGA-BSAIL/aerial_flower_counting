6b8e9db896e71bf85edda1b1ccfe1808ad7ee553
Daniel Petti <djpetti@gmail.com>
2021-01-31 11:27:38 -0500

    Fix LeNet architecture.
    I realized I had read the figure in the TasselNet paper slightly wrong.

--- src/cotton_counter/model/counting_model.py
+++ src/cotton_counter/model/counting_model.py
@@ -112,8 +112,8 @@ def _build_le_net_backbone(normalized_input: tf.Tensor) -> tf.Tensor:
     conv2_1 = _conv_bn_relu(32, 3, padding="same")(pool1)
     pool2 = layers.MaxPool2D()(conv2_1)
 
-    conv3_1 = _conv_bn_relu(64, 3, padding="same")(pool2)
-    conv3_2 = _conv_bn_relu(64, 3, padding="same")(conv3_1)
+    conv3_1 = _conv_bn_relu(64, 8, padding="same")(pool2)
+    conv3_2 = _conv_bn_relu(64, 1, padding="same")(conv3_1)
 
     return conv3_2
 
@@ -203,7 +203,7 @@ def _build_model_backbone(*, image_input: keras.Input) -> tf.Tensor:
     float_images = tf.cast(image_input, K.floatx())
     normalized = tf.image.per_image_standardization(float_images)
 
-    return _build_vgg_backboune(normalized)
+    return _build_le_net_backbone(normalized)
 
 
 def _build_density_map_head(model_top: tf.Tensor) -> tf.Tensor:
